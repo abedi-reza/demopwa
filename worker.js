d1c930254e99f48d87f62893df3c1e6273e8b0aa
Reza <reza.abi69@gmail.com>
2019-12-05 18:40:52 +0330

    Updates

---
+++ worker.js
@@ -0,0 +1,56 @@
+var CACHE_NAME = 'bidopin-pwa';
+var urlsToCache = [
+  '/',
+  '/index.html', 
+  '/manifest.json',
+  '/images/icons-512.png',
+  '/favicon.ico',
+  '/static/js/bundle.js',
+  'static/js/main.chunk.js',
+  '/static/js/0.chunk.js'
+
+
+];
+
+// Install a service worker
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+  var cacheWhitelist = ['bidopin-pwa'];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
